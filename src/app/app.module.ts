04326ea7a6e4dc0622ef5ed29579a0d1686fe84f
Brandonqr <brandonqr1@gmail.com>
2017-06-14 00:52:30 +0200

    agregada map en evento form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { LoginRegistroModalComponent } from './components/login-registro-modal/l
 import { PerfilComponent } from './components/perfil/perfil.component';
 import { MiPerfilComponent } from './components/perfil/mi-perfil/mi-perfil.component';
 import { EventoFormComponent } from './components/perfil/evento-form/evento-form.component';
+import { AgmCoreModule } from '@agm/core';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 //rutas
@@ -30,6 +31,9 @@ import { imgDefaultPipe } from "./pipes/imageDefault.pipe";
     imgDefaultPipe
   ],
   imports: [
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDSk0wgg8zV26dmBb6LrcNsT9oxa8Hk4fs'
+    }),
     BrowserModule,
     HttpModule,
     APP_ROUTING,
--- src/app/components/perfil/evento-form/evento-form.component.ts
+++ src/app/components/perfil/evento-form/evento-form.component.ts
@@ -1,14 +1,76 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule,  NgZone, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from "@angular/forms";
+import { ActivatedRoute } from  "@angular/router";
+import { MapsAPILoader } from '@agm/core';
+import {  } from '@types/googlemaps';
+
+declare let jQuery:any;
+declare let $:any;
+declare let google;
 
 @Component({
   selector: 'app-evento-form',
-  templateUrl: './evento-form.component.html'
+  templateUrl: './evento-form.component.html',
+  styles: []
 })
 export class EventoFormComponent implements OnInit {
 
-  constructor() { }
+
+
+eventoForm:FormGroup;
+evento:any={
+  nombre : "",
+  lugar : "",
+  descripcion:"",
+  fecha:"",
+  imagen:"",
+  autor : "",
+  categoria:""
+};
+lat: number = 51.678418;
+lng: number = 7.809007;
+  constructor(
+    private ngZone: NgZone
+   ) { }
 
   ngOnInit() {
+    //formulario, validar y otras cosas
+
+    this.eventoForm=new FormGroup({
+     'nombre': new FormControl('', [Validators.required, Validators.minLength(5)]),
+     'descripcion': new FormControl('', Validators.required),
+     'imagen': new FormControl( Validators.required),
+     'fecha': new FormControl(Validators.required),
+     'categoria': new FormControl(Validators.required)
+   })
+
+    //set current position
+    this.setCurrentPosition();
+
+
   }
+  private setCurrentPosition() {
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.lat = position.coords.latitude;
+      this.lng = position.coords.longitude;
+    });
+  }
+}
+   crearQuack(){
+
+  }
+
+dragEndMarcador( marcador:any, evento ){
+  let lat= evento.coords.lat;
+  let lng = evento.coords.lng;
+
+  marcador.lat = lat;
+  marcador.lng=lng;
+
+  //actualizar posicion del marcador
+  this.lat=lat;
+  this.lng=lng;
+}
 
 }
